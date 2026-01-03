3e518c5188020908794e2dbd49e2b8db9005e5f5
Ahmad-Rofiki <ahmadrofiki6146@gmail.com>
2026-01-03 20:44:08 +0700

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbars from "./components/Navbars";
-import Hero from "./components/Hero";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
